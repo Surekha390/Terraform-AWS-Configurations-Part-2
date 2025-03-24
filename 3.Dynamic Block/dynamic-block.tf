4a23825b3df9a834e8245830287f96ba51646be5
Surekha390 <surekha.tallapalli@gmail.com>
2025-03-24 18:29:28 +0100

    used dynamic block along with iterator for security group, inbound and outbound rules

---
+++ 3.Dynamic Block/dynamic-block.tf
@@ -0,0 +1,31 @@
+variable "sg_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8200, 8201,8300, 9200, 9500]
+}
+
+resource "aws_security_group" "dynamicsg" {
+  name        = "dynamic-sg"
+  description = "Ingress for Vault"
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port  #when we don't give this we have to write ingress.value instead of port.value in lines 15 and 16. 
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.sg_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
