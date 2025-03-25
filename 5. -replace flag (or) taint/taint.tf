f768555984d2ae139cb4720cf0cc98109d7de955
Surekha <surekha.tallapalli@gmail.com>
2025-03-25 07:14:58 +0100

    Delete 5. -replace flag (or) taint directory

--- 5. -replace flag (or) taint/taint.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_instance" "myec2" {
-    ami = "ami-0b6d6dacf350ebc82"
-    instance_type = "t2.micro"
-}
-
-# terraform apply -replace="aws_instance.myec2"   This command is used to replace the existing resource.
-#In older versions terraform taint was there, which also replaces the resource.
