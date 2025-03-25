091d2bd8a972d87898d1608017bae3c67d3942b5
Surekha390 <surekha.tallapalli@gmail.com>
2025-03-25 05:53:51 +0100

    code contains errors, when we run terraform validate, it throws errors.

---
+++ 4.terraform-validate/terraform-validate.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = var.instancetype  #throws error
+  sky = "blue" #throws error
+}
+
+#we have to run terraform validate and it checks for syntax errors and throws errors.
+
+#when we add instance_type = var.instance_type and when we don’t have variables section with instance_type, then it throws an error, 
+#Reference to undeclared input variable.
+
+#when we add sky = “blue” argument in aws_instance resource, and when we run terraform validate, it throws an error stating that “unsupported argument”
